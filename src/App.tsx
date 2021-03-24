2cc0a41e625e6c7dab8245f6600eaaad60a9dca2
ethan sayagh <ethan.sayagh@gmail.com>
2021-03-24 22:08:33 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch,Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Grid from './pages/Grid/Grid';
-import Header from "./pages/Header/Header"
+import Header from './pages/Header/Header';
+import Responsive from './pages/Responsive/Responsive';
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <hr />
       <Switch>
         <Route path='/' exact>
@@ -17,7 +18,10 @@ const App = () => {
         <Route path='/Grid' exact>
           <Grid />
         </Route>
-        <Redirect to="/" />
+        <Route path='/Responsive' exact>
+          <Responsive />
+        </Route>
+        <Redirect to='/' />
       </Switch>
     </BrowserRouter>
   );

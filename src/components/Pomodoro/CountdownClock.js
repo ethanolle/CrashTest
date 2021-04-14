534fc21595a2e8fe32d8c24968436fdc971d3de7
ethan sayagh <ethan.sayagh@gmail.com>
2021-04-14 19:58:57 +0300

    added counter + button

--- src/components/Pomodoro/CountdownClock.js
+++ src/components/Pomodoro/CountdownClock.js
@@ -1,11 +1,23 @@
 import './CountDownClock.css';
-import { useState } from 'react';
+// import { useState, useEffect } from 'react';
+import Countdown from 'react-countdown';
 
 const CountDownClock = () => {
-  const [minutesLeft, setMinutesLeft] = useState(25);
   return (
     <div className='CountDownClockContainer'>
-      <h1 className='this is the countDown'>{minutesLeft}</h1>
+      <div className='counterContainer'>
+        <Countdown
+          className='counterNum'
+          date={Date.now() + 1500000}
+          intervalDelay={0}
+          precision={3}
+          renderer={(props) => (
+            // <div>{props.total}</div> To Put it in ms
+            <div>{Math.floor(props.total / 60000)}</div>
+          )}
+        />
+        <button></button>
+      </div>
     </div>
   );
 };

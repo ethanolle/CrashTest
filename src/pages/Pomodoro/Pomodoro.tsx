ad8381e45b4727dfd07903a5c58f2afd75433461
ethan sayagh <ethan.sayagh@gmail.com>
2021-04-15 12:01:49 +0300

    centered and moved files

--- src/pages/Pomodoro/Pomodoro.tsx
+++ src/pages/Pomodoro/Pomodoro.tsx
@@ -1,9 +1,29 @@
-import './Pomodoro.css';
-import CountDownClock from '../../components/Pomodoro/CountdownClock';
+import './Pomodoro.scss';
+// import { useState, useEffect } from 'react';
+import Countdown from 'react-countdown';
+
 const Pomodoro = () => {
   return (
-    <div className='PomodoroContainer'>
-      <CountDownClock />
+    <div className='pomodoroContainer'>
+      <div className='containerCounter'>
+        <div className='counterContainer'>
+          <Countdown
+            className='counterNum'
+            date={Date.now() + 1500000}
+            intervalDelay={0}
+            precision={3}
+            renderer={(props) => (
+              // <div>{props.total}</div> To Put it in ms
+              <div>{Math.floor(props.total / 60000)}</div>
+            )}
+          />
+        </div>
+        <div className='buttonContainer'>
+          <a href='https://portfolioreactive.web.app/' className='btn btn-5'>
+            Start
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

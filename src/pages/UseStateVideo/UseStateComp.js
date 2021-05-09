80466b3b5e58f70a76ddb038917c7e142f808672
ethan sayagh <ethan.sayagh@gmail.com>
2021-05-09 19:00:12 +0300

    added comment of useState video

--- src/pages/UseStateVideo/UseStateComp.js
+++ src/pages/UseStateVideo/UseStateComp.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
 // import "./UseStateComp.css";
 
 const UseStateComp = () => {
-  const [number, setNumber] = useState({ ethan: "TheKing", coders: "Boss!" });
-  const handleIncrement = () => {
-    setNumber({ ...number, ethan: "the crazy KING" });
+  const [object, setObject] = useState({ ethan: 'TheKing', coders: 'Boss!' });
+  const [counter, setCounter] = useState(0);
+  const handleObject = () => {
+    //This is very bad
+    //setObject({  ethan: 'the crazy KING' })
+    setObject({ ...object, ethan: 'the crazy KING' });
   };
 
+  const handleCounter = () => {
+    //This is very bad
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
   return (
     <div className='App'>
-      <button onClick={handleIncrement}>Change</button>
-      <h5>{number.ethan}</h5>
-      <h5>{number.coders}</h5>
+      <button onClick={handleObject}>Change</button>
+      <h5>{object.ethan}</h5>
+      <h5>{object.coders}</h5>
+      <h3>{counter}</h3>
+      <button onClick={handleCounter}>+</button>
     </div>
   );
 };
